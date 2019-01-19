22daf1f918d3c816d079df591ca5b455b53ef570
lisniuse <17560235@qq.com>
2019-01-19 15:51:40 +0800

    chore: update the default theme.

--- packages/@teadocs/theme-default/src/pages/base/base.js
+++ packages/@teadocs/theme-default/src/pages/base/base.js
@@ -2,3 +2,20 @@
 import '../../assets/css/reset.css';
 import '../../assets/css/public.less';
 import '../../assets/css/style.less';
+import '../../assets/css/icon.less';
+import '../../assets/css/ant-markdown.css';
+import Vue from 'vue/dist/vue.js'
+//const Vue = require('vue');
+let baseApp = new Vue({
+    el: '#app',
+    data() {
+        return {
+            nav: {
+                isShow: false
+            }
+        }
+    },
+    methods: {
+
+    }
+});
