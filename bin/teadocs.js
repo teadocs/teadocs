97821d16f70f3042c2b8d4a8c4b34e6fc10dffb9
ZhiBing(陈治兵) <l@numpy.org.cn>
2019-11-12 10:07:55 +0800

    fix: 修改bin\teadocs的缩进，增加测试目录。

--- bin/teadocs.js
+++ bin/teadocs.js
@@ -5,12 +5,12 @@ const semver = require('semver')
 const requiredVersion = require('../package.json').engines.node
 
 if (!semver.satisfies(process.version, requiredVersion)) {
-    console.log(chalk.red(
-        `\n[teadocs] minimum Node version not met:` +
-        `\nYou are using Node ${process.version}, but Teadocs ` +
-        `requires Node ${requiredVersion}.\nPlease upgrade your Node version.\n`
-    ))
-    process.exit(1)
+  console.log(chalk.red(
+    `\n[teadocs] minimum Node version not met:` +
+    `\nYou are using Node ${process.version}, but Teadocs ` +
+    `requires Node ${requiredVersion}.\nPlease upgrade your Node version.\n`
+  ))
+  process.exit(1)
 }
 
 const path = require('path')
@@ -21,49 +21,49 @@ const init = require('../lib/init')
 const program = require('commander')
 
 program
-    .version(require('../package.json').version)
-    .usage('<command> [options]')
+  .version(require('../package.json').version)
+  .usage('<command> [options]')
 
 program
-    .command('dev [targetDir]')
-    .description('start development server and listen for file changes automatically compiled.')
-    .option('-p, --port <port>', 'use specified port (default: 3210)')
-    .option('-h, --host <host>', 'use specified host (default: 0.0.0.0)')
-    .action((dir = '.', { host, port }) => {
-        new dev.Dev({ host, port }).start()
-    })
+  .command('dev [targetDir]')
+  .description('start development server and listen for file changes automatically compiled.')
+  .option('-p, --port <port>', 'use specified port (default: 3210)')
+  .option('-h, --host <host>', 'use specified host (default: 0.0.0.0)')
+  .action((dir = '.', { host, port }) => {
+    new dev.Dev({ host, port }).start()
+  })
 
 program
-    .command('build [targetDir]')
-    .description('build dir as static site')
-    .option('-d, --dest <outDir>', 'specify build output dir (default: ./build)')
-    .action((dir = '.', { dest }) => {
-        const outDir = dest ? path.resolve(dest) : null
-        new build.Build({ outDir }).build()
-    })
+  .command('build [targetDir]')
+  .description('build dir as static site')
+  .option('-d, --dest <outDir>', 'specify build output dir (default: ./build)')
+  .action((dir = '.', { dest }) => {
+    const outDir = dest ? path.resolve(dest) : null
+    new build.Build({ outDir }).build()
+  })
 
 program
-    .command('init [targetDir]')
-    .description('generate documents required in an empty directory')
-    .action((dir = '.') => {
-        console.log(dir);
-        init(path.resolve(dir))
-    })
+  .command('init [targetDir]')
+  .description('generate documents required in an empty directory')
+  .action((dir = '.') => {
+    console.log(dir);
+    init(path.resolve(dir))
+  })
 
 // output help information on unknown commands
 program
-    .arguments('<command>')
-    .action((cmd) => {
-        program.outputHelp()
-        console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
-        console.log()
-    })
+  .arguments('<command>')
+  .action((cmd) => {
+    program.outputHelp()
+    console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+    console.log()
+  })
 
 // add some useful info on help
 program.on('--help', () => {
-    console.log()
-    console.log(`  Run ${chalk.cyan(`teadocs <command> --help`)} for detailed usage of given command.`)
-    console.log()
+  console.log()
+  console.log(`  Run ${chalk.cyan(`teadocs <command> --help`)} for detailed usage of given command.`)
+  console.log()
 })
 
 program.commands.forEach(c => c.on('--help', () => console.log()))
@@ -71,14 +71,14 @@ program.commands.forEach(c => c.on('--help', () => console.log()))
 program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
-    program.outputHelp()
-    process.exit(1)
+  program.outputHelp()
+  process.exit(1)
 }
 
 function wrapCommand(fn) {
-    return (...args) => {
-        return fn(...args).catch(err => {
-            console.error(chalk.red(err.stack))
-        })
-    }
+  return (...args) => {
+    return fn(...args).catch(err => {
+      console.error(chalk.red(err.stack))
+    })
+  }
 }
